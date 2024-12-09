49e96df8494b1c504f62fd7b87c48ca940dfb872
Salty <2willjoesalt2@gmail.com>
2024-12-09 07:38:23 -0700

    addding to swerve auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive/SwerveKit/SwerveDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive/SwerveKit/SwerveDrive.java
@@ -2,13 +2,16 @@ package org.firstinspires.ftc.teamcode.Drive.SwerveKit;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.PoseVelocity2d;
 import com.acmerobotics.roadrunner.ftc.DownsampledWriter;
 import com.acmerobotics.roadrunner.ftc.LynxFirmware;
 import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.teamcode.Util.nov25swerve.AdvancedSwerveDrivetrain;
+
 @Config
-public final class MecanumDrive {
+public final class SwerveDrive {
 
     //Parameters Section
     // drive model parameters
@@ -20,6 +23,8 @@ public final class MecanumDrive {
     public double kS = 0;
     public double kV = 0;
     public double kA = 0;
+
+    AdvancedSwerveDrivetrain drivetrain;
 
     // path profile parameters (in inches)
     public double maxWheelVel = 50;
@@ -54,9 +59,9 @@ public final class MecanumDrive {
     private final DownsampledWriter estimatedPoseWriter = new DownsampledWriter("ESTIMATED_POSE", 50_000_000);
     private final DownsampledWriter targetPoseWriter = new DownsampledWriter("TARGET_POSE", 50_000_000);
     private final DownsampledWriter driveCommandWriter = new DownsampledWriter("DRIVE_COMMAND", 50_000_000);
-    private final DownsampledWriter mecanumCommandWriter = new DownsampledWriter("MECANUM_COMMAND", 50_000_000);
+    private final DownsampledWriter swerveCommandWriter = new DownsampledWriter("SWERVE_COMMAND", 50_000_000);
 
-    public MecanumDrive(HardwareMap hardwareMap, Pose2d pose){
+    public SwerveDrive(HardwareMap hardwareMap, Pose2d pose){
         this.pose = pose;
 
         LynxFirmware.throwIfModulesAreOutdated(hardwareMap);
@@ -65,7 +70,13 @@ public final class MecanumDrive {
             module.setBulkCachingMode(LynxModule.BulkCachingMode.AUTO);
         }
 
+        drivetrain = new AdvancedSwerveDrivetrain(hardwareMap);
 
+        //localizer
+
+    }
+
+    public void setDrivePowers(PoseVelocity2d powers){
 
     }
 
